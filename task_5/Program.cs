e7c7b3340891dd6ce943cb00d7bd9d3c507ab84f
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-11-30 20:34:49 +0300

    task_5

--- task_5/Program.cs
+++ task_5/Program.cs
@@ -1,9 +1,22 @@
 ﻿// Напишите программу, считывающую с пользовательского ввода целое число n (неотрицательное), выводящее это число в консоль вместе с правильным образом изменённым словом "программист", для того, чтобы робот мог нормально общаться с людьми, например: 1 программист, 2 программиста, 5 программистов.
 Console.WriteLine("Введите количество программистов");
+try
+{
+
 int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-string res = String.Empty;
-if (num%100>=10 && num%100<=14) res="ов";
-else if (num%10>=2 &&num%10<=4) res="а";
-else if (num%10==1) res="";
-else res="ов";
-Console.WriteLine(num+" программист"+res);
+numbProg(num);
+}
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
+
+void numbProg(int arg)
+{
+    string res = String.Empty;
+    if (arg % 100 >= 10 && arg % 100 <= 14) res = "ов";
+    else if (arg % 10 >= 2 && arg % 10 <= 4) res = "а";
+    else if (arg % 10 == 1) res = "";
+    else res = "ов";
+    Console.WriteLine(arg + " программист" + res);
+}
