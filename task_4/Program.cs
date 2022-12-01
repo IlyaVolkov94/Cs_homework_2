ac4017e2fdee5de8713719403e4ca8bca3af548b
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-01 22:19:02 +0300

    task_4_watch

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -4,25 +4,37 @@
 // Задача может показаться очевидной, но нужно получить наиболее простое и красивое решение.
 using System.Diagnostics;
 
-string result =string.Empty;
 string undRes =string.Empty;
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
 
 // Стихийный способ:
-for(int i=1;i<=100;i++)    
-{
-    if(i%3==0)
-    {
-       undRes=undRes+"Fizz";
-        if (i%5==0)  undRes=undRes+"Buzz";
-    }
-    else if (i%5==0) undRes=undRes+"Buzz";
-    else undRes=undRes+i;
-    result=result+undRes+" ";
-    undRes=string.Empty;
-}
-Console.Write(result);
+// string result =string.Empty;
+// for(int i=1;i<=100;i++)    
+// {
+//     if(i%3==0)
+//     {
+//        undRes=undRes+"Fizz";
+//         if (i%5==0)  undRes=undRes+"Buzz";
+//     }
+//     else if (i%5==0) undRes=undRes+"Buzz";
+//     else undRes=undRes+i;
+//     result=result+undRes+" ";
+//     undRes=string.Empty;
+// }
+// Console.Write(result);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
